e84cbc048b9a207d3f02cad5b8f6584be5b4884c
Darshana Perera <darshana.saluka.10@gmail.com>
2022-06-26 21:47:18 +0530

    material ui added

--- src/Design/Design.js
+++ src/Design/Design.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import Details from "./Details/Details";
+import Menu from "./Menu/Menu";
 
 export default function Design() {
   return (
-    <div>Design</div>
-  )
+    <div style={{ display: "flex",width:"100%" }}>
+      <Menu style={{width:"300vh"}}/>
+      <Details />
+    </div>
+  );
 }
