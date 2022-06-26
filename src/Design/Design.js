a81fc65c3e496157bdda7776e23baf01332e0db0
Darshana Perera <darshana.saluka.10@gmail.com>
2022-06-26 22:03:09 +0530

    grids added

--- src/Design/Design.js
+++ src/Design/Design.js
@@ -2,11 +2,19 @@ import React from "react";
 import Details from "./Details/Details";
 import Menu from "./Menu/Menu";
 
+import Grid from "@mui/material/Grid";
+
 export default function Design() {
   return (
-    <div style={{ display: "flex",width:"100%" }}>
-      <Menu style={{width:"300vh"}}/>
-      <Details />
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4} md={4} lg={3}>
+          <Menu />
+        </Grid>
+        <Grid item xs={12} sm={8} md={8} lg={9}>
+          <Details />
+        </Grid>
+      </Grid>
     </div>
   );
 }
