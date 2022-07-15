c7c80a3fc6107775b96304ac2bf79cabe6b018e3
Darshana Perera <darshana.saluka.10@gmail.com>
2022-07-15 20:15:12 +0530

    structure created

--- src/Designs/Designs.js
+++ src/Designs/Designs.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import BottomBar from "./BottomBar/BottomBar";
+import Middle from "./Middle/Middle";
+import TopBar from "./TopBar/TopBar";
 
 export default function Designs() {
   return (
-    <div>Designs</div>
-  )
+    <div>
+      <TopBar />
+      <Middle />
+      <BottomBar />
+    </div>
+  );
 }
